3f925da8bfe825c00d6708138f005d2081ea19ea
RakaYulia <vegoraka7887@gmail.com>
2026-03-10 16:37:26 +0700

    Add files via upload

---
+++ 109082500201_Raka-Yulia-Vegora/Week 2 - Modul 2/109082500201_Raka-Yulia-Vegora/Week 3 - Modul 3/Guided/Guided-1/Guided1.go
@@ -0,0 +1,17 @@
+package main
+import"fmt"
+
+func tambahValue(x, int){
+	x = x + 10
+	fmt.Println("Nilai x didalam prosedur tambahValue (pass by value): ", x)
+}
+
+func tambahReference(x *int){
+	*x = *x + 10
+	fmt.Println("Nilai x didalam prosdur tambahRefernce (pass by reference): ", *x)
+}
+
+func main(){
+	var y int = 5
+	fmt.Println(Nilai y awal: ,y)
+}}
---
+++ 109082500201_Raka-Yulia-Vegora/Week 2 - Modul 2/109082500201_Raka-Yulia-Vegora/Week 3 - Modul 3/Guided/Guided-2/Guided2.go
@@ -0,0 +1,13 @@
+package main
+import"fmt"
+
+func hitungTotalBelanja(harga int, jumlah int){
+	var total int
+	total = harga * jumlah
+	fmt.Println("total harga : ", total )	
+}
+
+func hitungDiskon(total int){
+	var diskon, hargaAkhir int
+	
+}
